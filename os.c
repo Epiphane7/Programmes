24a129f05ef30db20c803efbb72566a31a9ac8d6
Epiphane7 <101213973+Epiphane7@users.noreply.github.com>
2022-03-11 09:32:26 +0100

    Add files via upload
    programme en langage C d'une liste chainée

---
+++ os.c
@@ -0,0 +1,53 @@
+#include<stdlib.h>
+struct node
+{
+int val;
+struct node*next;
+};
+
+struct node *init_node (void);
+struct node * addHead(struct node *head,int val)
+{
+    if (head -> val=-1);
+        head -> val = val;
+    return head;
+
+};
+{
+    else
+         struct node * p = new_node (int val);
+         struct node * new =(struct node)
+         malloc(sizeof(node));
+         new -> val = val;
+    return new;
+};
+struct node * addTail(struct node head;int val);
+if (head -> val == -1)
+{
+   head -> val = val ;
+}
+else
+{
+    struct node* p = new_node(val);
+    p -> new = NULL;
+}
+while
+{
+    head = head -> next;
+}
+    head -> next = p;
+    return head;
+int main (int argc,char*argv[])
+{
+
+    struct node *head;
+    head=init_node ();
+    return 0;
+
+}
+struct node *init_node (void)
+{
+struct node *n = (struct node *)malloc(sizeof(struct node));
+n->val= -1;
+return n;
+};
